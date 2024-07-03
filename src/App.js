fe6cc91cdea31fdddc4fbfc92107c30b382092a4
Alma-Yovell <alma.yovell@gmail.com>
2024-07-03 14:34:04 +0300

    custme changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img
+          src={process.env.PUBLIC_URL + '/collegeLogo.png'} alt="Decorative" className="App-logo" />
+
+        <p className="title">
+          react לומדת
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with me
-        </a>
       </header>
     </div>
   );
